9c75f6cb5310c43e63916a419e6eb39c21ca2547
Mukul <mukulgupta393@gmail.com>
2024-12-26 16:27:29 +0530

    Different pages created

--- src/App.js
+++ src/App.js
@@ -1,55 +1,20 @@
 import './App.css';
+import Post from './post';
+import Header from './Header';
+import { Route, Routes } from "react-router-dom";
+import Layout from './Layout';
+import IndexPage from './pages/indexpage';
+import LoginPage from './pages/LoginPage';
+
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2024/07/GettyImages-2158243831.jpg?resize=1280,720" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Elon Musk’s xAI lands $6B in new cash to fuel AI ambitions</h2>
-          <p className="info">
-            <a className="author">Mukul Gupta</a>
-            <time>24-2024-12 16:45</time>
-          </p>
-          <p className="summary">xAI, Elon Musk’s AI company, has raised $6 billion, according to a filing with the U.S. Securities and Exchange Commission on Thursday.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2024/07/GettyImages-2158243831.jpg?resize=1280,720" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Elon Musk’s xAI lands $6B in new cash to fuel AI ambitions</h2>
-          <p className="info">
-            <a className="author">Mukul Gupta</a>
-            <time>24-12-2024 16:45</time>
-          </p>
-          <p className="summary">xAI, Elon Musk’s AI company, has raised $6 billion, according to a filing with the U.S. Securities and Exchange Commission on Thursday.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2024/07/GettyImages-2158243831.jpg?resize=1280,720" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Elon Musk’s xAI lands $6B in new cash to fuel AI ambitions</h2>
-          <p className="info">
-            <a className="author">Mukul Gupta</a>
-            <time>24-2024-12 16:45</time>
-          </p>
-          <p className="summary">xAI, Elon Musk’s AI company, has raised $6 billion, according to a filing with the U.S. Securities and Exchange Commission on Thursday.</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />}/>
+        <Route path="/login" element={<LoginPage/>} />
+      </Route>
+    </Routes>
   );
 }
 
